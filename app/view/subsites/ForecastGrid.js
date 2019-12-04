1a50b69b417fad1ffc94e63e2288ff3ab73c49a7
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-12-04 13:33:01 +0100

    Setting up a conditional images in rowexpand

--- app/view/subsites/ForecastGrid.js
+++ app/view/subsites/ForecastGrid.js
@@ -40,20 +40,21 @@ Ext.define('ExtWeather.view.subsites.ForecastGrid', {
             ptype: 'rowexpander',
             rowBodyTpl: [
                 '<tpl for=".">', // Ten for to potrzebny, bez tego nie dorwie storage'u root'a
+                    '<tpl if="temp &lt;=273.15">',
+                        '<img src="/resources/winter.jpg"/>',
+                    '<tpl elseif="temp &gt;=273.15 &amp;&amp; temp &lt;=278.15">', // zmienne z root'a są osiągalne tu jako nazwa zmiennej.
+                        '<img src="/resources/autumn.jpg"/>',
+                    '<tpl elseif="temp &gt;278.15 &amp;&amp; temp &lt;=288.15">',
+                        '<img src="/resources/spring.jpg"/>',
+                    '<tpl elseif="temp &gt;278.15">',    
+                        '<img src="/resources/summer.jpg"/>',
+                    '</tpl>',
                     "<p>We will have: <b>{description}</b></p>", // Te klamry się odnoszą do Storage!
                     "<p>Minimal Temperature : <b id='min'>{temp_min}</b></p>",
                     "<p>Maximal Temperature : <b>{temp_max}</b></p>",
-                    "<br />Wind will blow at: <b>{wind_deg}\u00B0</b> with velocity of: <b>{wind_speed}</b> km/h",
+                    "<br />Wind will blow at: <b>{wind_deg}\u00B0</b> with velocity of: <b>{wind_speed}</b> km/h<hr>",
                     // CONDITIONAL:
-                    '<tpl if="temp &lt;=273.15">',
-                        '<p>Winter</p>',
-                    '<tpl elseif="temp &gt;=273.15 &amp;&amp; temp &lt;=278.15">', // zmienne z root'a są osiągalne tu jako nazwa zmiennej.
-                        '<p>Autumn</p>',
-                    '<tpl elseif="temp &gt;278.15 &amp;&amp; temp &lt;=288.15">',
-                        '<p>Spring</p>',
-                    '<tpl elseif="temp &gt;278.15">',    
-                        '<p>Summer</p>',
-                    '</tpl>',         
+                             
                 '</tpl>'
             ],
             rowexpander: true,
